f4483a84b0c5158938e79b6378873bcb44e5673c
Hugo López Fernández <hlfernandez@uvigo.es>
2014-12-03 12:33:27 +0100

    Upgrade the RUbioSeq-GUI launching script
    Update launch script to:
    - Store the pid of the "java -jar jetty-runner.jar [...]" process into a
    server.pid file and the port used to deploy the application into a
    server.port file.
    - Detect if an instance of RUbioSeq-GUI is deployed (i.e. the pid stored
    in the server.pid file corresponds to a process that is running).
    - Detect if the port is un use. If the port is in use, the application
    is not re-deployed and only the local browser is openned.

--- src/main/resources/scripts/launch-rubioseq-gui.sh
+++ src/main/resources/scripts/launch-rubioseq-gui.sh
@@ -9,9 +9,9 @@ usage: $0 options
 This script runs jetty-runner to deploy rubioseq-gui.war and then launchs the default browser.
 
 OPTIONS:
-   -h      Show this message
-   -w      war to deploy. If no specified, rubioseq-gui.war is used.
-   -p      Port used to deploy the server. If no specified, 8080 is used.   
+   -h	Show this message
+   -w	war to deploy. If no specified, rubioseq-gui.war is used.
+   -p	Port used to deploy the server. If no specified, 8080 is used.
 EOF
 }
 
@@ -32,9 +32,11 @@ WAR="${SCRIPTDIR}/rubioseq-gui.war"
 WORKDIR="${HOME}/.local/share/rubioseq-gui"
 JAVAPARAMS="-Xmx4G"
 PORT="8080"
-OPEN="xdg-open";
+OPEN="xdg-open"
+SERVERPID="${WORKDIR}/server.pid"
+SERVERPORT="${WORKDIR}/server.port"
 
-while getopts “hw:p:” OPTION
+while getopts “hw:p:f” OPTION
 do
      case $OPTION in
          h)
@@ -64,18 +66,41 @@ case "$OSTYPE" in
 	  echo "[$SCRIPT] (O.S. detection)[Could not determine the host O.S.: Using command 'xdg-open' to open the main page of the app." ;;
 esac
 
+if [ -f "$SERVERPID" ]
+then
+  PIDCHECK=`cat $SERVERPID`;
+  if ps -p $PIDCHECK > /dev/null
+  then
+    echo "[$SCRIPT] One instance of RUbioSeq-GUI is already running (pid=$PIDCHECK)." 
+    if [ -f "$SERVERPORT" ]
+    then
+      PORT=`cat $SERVERPORT`;
+      $OPEN http://localhost:$PORT/login.zul
+    fi
+    exit 0
+  fi
+fi
+
+if netstat -n --listening | grep ":$PORT" > /dev/null
+then
+  echo "[$SCRIPT] Port $PORT is already in use by other application. Please, use option -p <port> to specify a free port."
+  exit -1
+fi
+
 if [ -f "$WAR" ]
 then
   mkdir -p "$WORKDIR"
   cd "$WORKDIR"
-  java $JAVAPARAMS -jar "$SCRIPTDIR"/jetty-runner-8.1.9.v20130131.jar --port $PORT "$WAR" | while read line; do
+  ( java $JAVAPARAMS -jar "$SCRIPTDIR"/jetty-runner-8.1.9.v20130131.jar --port $PORT "$WAR" & echo $! >&3 ) 3>$SERVERPID | while read line; do
     echo $line;
     case "$line" in 
       *"DB initialized"*)
 	$OPEN http://localhost:$PORT/login.zul
 	;;
     esac  
-   done
+  done &
+  echo $PORT > $SERVERPORT;
+  swait
 else
   echo "[$SCRIPT] $WAR does not exist.";
 fi
